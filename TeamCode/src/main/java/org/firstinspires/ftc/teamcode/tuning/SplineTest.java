050e7a2942ad9333457c4481849f06e0e91566b7
Alespic26 <153930576+Alespic26@users.noreply.github.com>
2025-03-20 16:23:55 +0100

    Logic for BeginPose X and Y

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/SplineTest.java
@@ -1,12 +1,4 @@
 package org.firstinspires.ftc.teamcode.tuning;
-import org.firstinspires.ftc.ftccommon.internal.manualcontrol.parameters.ImuParameters;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -17,10 +9,7 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import static java.lang.Math.sqrt;
 
-import java.lang.annotation.Target;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
@@ -57,12 +46,12 @@ public final class SplineTest extends LinearOpMode {
 
         //TODO: Method this shit man wth
         Dictionary<Integer, AprilTagInformation> aprilTagDict = new Hashtable<>();
-        aprilTagDict.put(11,new AprilTagInformation(-48,-72, false));
-        aprilTagDict.put(12,new AprilTagInformation(-72,0, true));
-        aprilTagDict.put(13,new AprilTagInformation(-48,+72, false));
-        aprilTagDict.put(14,new AprilTagInformation(+48,+72, false));
-        aprilTagDict.put(15,new AprilTagInformation(+72,+0, true));
-        aprilTagDict.put(16,new AprilTagInformation(+48,-72, false));
+        aprilTagDict.put(11,new AprilTagInformation(-48,-72, 0));
+        aprilTagDict.put(12,new AprilTagInformation(-72,0, 90));
+        aprilTagDict.put(13,new AprilTagInformation(-48,+72, 180));
+        aprilTagDict.put(14,new AprilTagInformation(+48,+72, 180));
+        aprilTagDict.put(15,new AprilTagInformation(+72,+0, 270));
+        aprilTagDict.put(16,new AprilTagInformation(+48,-72, 0));
 
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         AprilTagDetection TargetTag;
@@ -96,45 +85,62 @@ public final class SplineTest extends LinearOpMode {
         else{
             double beginPoseX;
             double beginPoseY;
-            if(TargetTag.ftcPose.yaw > 0){
-                if(targetTagInfo.invertXY){
-                    beginPoseY = targetTagInfo.xPos + xToTargetTag;
-                    beginPoseX = targetTagInfo.yPos + yToTargetTag;
-                }
-                else{
-                    beginPoseX = targetTagInfo.xPos + xToTargetTag;
-                    beginPoseY = targetTagInfo.yPos + yToTargetTag;
-                }
-                //X needs to be added.
-            }
-            else if(TargetTag.ftcPose.yaw < 0){
-                if(targetTagInfo.invertXY){
-                    beginPoseY = targetTagInfo.xPos + xToTargetTag;
-                    beginPoseX = targetTagInfo.yPos + yToTargetTag;
-                }
-                else{
-                    beginPoseX = targetTagInfo.xPos + xToTargetTag;
-                    beginPoseY = targetTagInfo.yPos + yToTargetTag;
-                }
-                //X needs to be subtracted
-            }
-            Pose2d beginPose = new Pose2d(0, 0, 0);
+            boolean isAddition = checkTagOp(targetTagInfo.angle);
+            boolean isXNegated = checkRelativeAng(targetTagInfo.angle);
+            boolean areAxisInverted = checkAxisInversion(targetTagInfo.angle);
+
+
+            xToTargetTag = (isXNegated)? xToTargetTag : -xToTargetTag;
+
+            double supportVar = xToTargetTag;
+            xToTargetTag = (areAxisInverted) ? yToTargetTag : xToTargetTag;
+            yToTargetTag = (areAxisInverted) ? supportVar : yToTargetTag;
+
+            beginPoseX = (isAddition) ? xToTargetTag + targetTagInfo.xPos : xToTargetTag - targetTagInfo.xPos;
+            beginPoseY = (isAddition) ? yToTargetTag + targetTagInfo.yPos : yToTargetTag - targetTagInfo.yPos;
+
+
+
+
+            //To calculate the heading you need to add both angles together and subtract by 180.
+            //TODO: normalize angle values so that they can be processed as radiant.
+            Pose2d beginPose = new Pose2d(beginPoseX, beginPoseY, 0);
 
         }
 
+    }
+    public boolean checkTagOp(int ang){
+        //If the tag is within the negative zone, the operator
+        //will be an addition due to the relativistic nature
+        //of the points of reference (AprilTags orientation)
+        return ang == 90 || ang == 0;
+    }
+    public boolean checkRelativeAng(int ang){
+        //If the yaw is positive, and therefore the robot
+        //is at the left of the AprilTag, the X vector
+        //will need to be negated
+        return ang <= 0;
+    }
+
+    public boolean checkAxisInversion(int ang){
+        //If the robot is looking sideways, the relative
+        //axis measurements and the global axis are flipped.
+        //RelX will be GlobY and vice versa.
+        return ang == 90 || ang == 270;
     }
 
 
 }
 
+
 class AprilTagInformation{
     int xPos;
     int yPos;
-    boolean invertXY;
+    int angle;
 
-    public AprilTagInformation(int x, int y, boolean invert) {
+    public AprilTagInformation(int x, int y, int ang) {
          xPos = x;
          yPos = y;
-         invertXY = invert;
+         angle = ang;
     }
 }
