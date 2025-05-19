fd586c269b8d47d1b6046e9b30b95b3406655fdc
Alespic26 <alespicm4ddino@gmail.com>
2025-05-19 15:08:00 +0200

    Update PinpointLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointLocalizer.java
@@ -33,7 +33,7 @@ public final class PinpointLocalizer implements Localizer {
         double mmPerTick = 25.4 * inPerTick;
         driver.setEncoderResolution(1 / mmPerTick);
         //driver.setOffsets(mmPerTick * PARAMS.parYTicks, mmPerTick * PARAMS.perpXTicks);
-        driver.setOffsets(58.0 * mmPerTick, -167.0 * mmPerTick);
+        driver.setOffsets(0.0044942734 * mmPerTick, 0.0129404079  * mmPerTick);
         // TODO: reverse encoder directions if needed
         initialParDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
         initialPerpDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
