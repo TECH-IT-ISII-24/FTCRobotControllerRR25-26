9f5fbbe6302abb8af0cf0539e20858a559923342
Alespic26 <alespicm4ddino@gmail.com>
2025-05-19 16:33:46 +0200

    New merge, fix twidth

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 0.6603695199107085;
         public double kV = 0.00036441088533463743;
-        public double kA = 0;
+        public double kA = 0.000001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
