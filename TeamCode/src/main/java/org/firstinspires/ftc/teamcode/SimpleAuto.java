016c8c24182832a97e2727d232aec33d9b024b03
Alespic26 <alespicm4ddino@gmail.com>
2026-03-06 15:20:24 +0100

    ubitech simple auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
@@ -14,19 +14,42 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class SimpleAuto extends LinearOpMode {
     @Override
             public void runOpMode(){
+
+
         waitForStart();
 
-        Pose2d beginPose = new Pose2d(63, 12, Math.PI);
+        //Pose2d beginPose = new Pose2d(63, 12, Math.PI);
+        Pose2d beginPose = new Pose2d(63, 24, Math.PI);
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+        DcMotorEx rampDrive = drive.rampDrive;
+        DcMotorEx shooterDrive = drive.shooterDrive;
+
+//        Actions.runBlocking(
+//                drive.actionBuilder(beginPose)
+//                        .strafeToLinearHeading(new Vector2d(-12, 12), Math.PI * 0.78)
+//                        .stopAndAdd(setDrive(rampDrive, 1))
+//                        .waitSeconds(1)
+//                        .stopAndAdd(setDrive(rampDrive, 0.93))
+//                        .waitSeconds(3.25)
+//                        .stopAndAdd(setDrive(rampDrive, 1))
+//                        .stopAndAdd(setDrive(rampDrive, 0.93))
+//                        .strafeToLinearHeading(new Vector2d(60,12), Math.PI)
+//                        .strafeTo(new Vector2d(60, 48))
+//                        .build()
+//        );
 
         Actions.runBlocking(
                 drive.actionBuilder(beginPose)
-                        .strafeToLinearHeading(new Vector2d(-12, 12), Math.PI * 0.78)
-                        .strafeToLinearHeading(new Vector2d(60,12), Math.PI)
-                        .strafeTo(new Vector2d(60, 48))
+                        .strafeTo(new Vector2d(60, 56))
                         .build()
         );
 
+    }
+    public Action setDrive(DcMotorEx motor, double power) {
+        return packet -> {
+            motor.setPower(power);
+            return false;
+        };
     }
 
 
